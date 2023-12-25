2e46f6ee863b7f2e50cf0ed0022c1e27e8778718
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2023-12-25 12:38:27 -0300

    test: rota /users de criação de usuários

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,3 +9,23 @@ def test_rota_raiz_deve_retornar_200_e_ola_mundo():
 
   assert response.status_code == 200
   assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_rota_user_deve_retornar_201_e_novo_usuario():
+  client = TestClient(app)
+
+  response = client.post(
+    '/users',
+    json={
+      'username': 'Geo',
+      'email': 'geo@example.com.br',
+      'password': 'batatinhas'
+    }
+  )
+
+  assert response.status_code == 201
+  assert response.json() == {
+    'id': 1,
+    'username': 'Geo',
+    'email': 'geo@example.com.br',
+  }
