617543706cf87a1b12183883ba1af6feadf86219
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2023-12-25 12:37:44 -0300

    feat: rota /users para criação de usuários com banco falso

--- fast_todo/app.py
+++ fast_todo/app.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 
+from fast_todo.schemas import UserPublic, UserSchema, UserDB
+
 app = FastAPI()
 
 
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+fake_database = [] # para estudo
+
+
+@app.post('/users', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(fake_database) + 1)
+    fake_database.append(user_with_id)
+
+    return user_with_id
